b246f341a6a9b11c3735c39e7234b679938ba44b
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-06-27 01:05:54 -0500

    added color components for each Link

---
+++ src/app/exercises/01-screensaver/blue/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScreenSaver from '../../../../components/ScreenSaver';
+
+function ScreenSaverExercise() {
+  return (
+    <main className="screen-saver-wrapper">
+      <ScreenSaver color="blue" />
+    </main>
+  );
+}
+
+export default ScreenSaverExercise;
--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Link from 'next/link';
 import ScreenSaver from '../../../components/ScreenSaver';
 
 function ScreenSaverExercise() {
   return (
     <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+      Choose Your Color:
+      <li><Link href="01-screensaver/red">Red</Link></li>
+      <li><Link href="01-screensaver/white">White</Link></li>
+      <li><Link href="01-screensaver/blue">Blue</Link></li>
     </main>
   );
 }
