87e74d2ccc75bd0aa53ca49abeb3a930ce210152
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-06-27 13:56:31 -0500

    added some things back and used onSubmit instead of onClick

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,27 +1,33 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import {ToastContext} from "../../../../components/ToastProvider";
 
 function ContactPage() {
   const router = useRouter();
+  const toastMessage = "Your message was received. We'll get back to you shorty";
+  const { createToast } = React.useContext(ToastContext);
 
   function handleSubmit(event){
     event.preventDefault();
     router.push("/exercises/02-flash-messages")
+    createToast(toastMessage, 'success');
   }
 
   return (
-    <main>
-      <form>
+    <><main>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
         <label htmlFor="message">Message:</label>
         <textarea id="message" />
 
-        <button onClick={handleSubmit}>Submit</button>
+        <button>Submit</button>
       </form>
     </main>
+    </>
+
   );
 }
 
